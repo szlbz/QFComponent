05738a479205f543b8fe5df2d09d2e7a30f9074d
szlbz <109887617+szlbz@users.noreply.github.com>
2024-03-09 08:26:50 +0800

    Add files via upload

---
+++ demo/unit3.pas
@@ -0,0 +1,51 @@
+unit unit3;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, DB, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  QFComponent, Ledger;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    QFRichView1: TQFRichView;
+    ScrollingText1: TQFScrollingText;
+    procedure FormCreate(Sender: TObject);
+    //procedure UniQuery1nameGetText(Sender: TField; var aText: string;
+    //  DisplayText: Boolean);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  ScrollingText1.Active:=true;
+  //UniConnection1.SpecificOptions.Values['SQL Server.Provider']:='prDirect';// .Add('SQL Server.Provider=prDirect');
+  //UniConnection1.Connected:=true;
+  //UniQuery1.Active:=true;
+end;
+
+//procedure TForm1.UniQuery1nameGetText(Sender: TField; var aText: string;
+//  DisplayText: Boolean);
+//begin
+//  aText:= CP936ToUTF8(Sender.Value);
+//end;
+
+end.
+
