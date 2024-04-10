a574f099b78c9e250d9d5a44f7608371825f010d
szlbz <109887617+szlbz@users.noreply.github.com>
2024-04-10 10:00:59 +0800

    Add files via upload

--- demo/unit3.pas
+++ demo/unit3.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, DB, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  ColorBox, ComCtrls, QFComponent;
+  ColorBox, ComCtrls, ComboEx, DBCtrls, EditBtn, QFComponent, Types;
 
 type
 
@@ -19,14 +19,27 @@ type
     Button4: TButton;
     Button5: TButton;
     Button6: TButton;
+    Button7: TButton;
+    DateEdit1: TDateEdit;
+    eeee: TEdit;
+    Label2: TLabel;
+    LabeledEdit1: TLabeledEdit;
+    LabeledEdit2: TLabeledEdit;
+    Memo1: TMemo;
+    Memo2: TMemo;
+    Panel3: TPanel;
+    tel: TEdit;
+    name3: TButton;
     CheckBox1: TCheckBox;
     CheckBox2: TCheckBox;
     CheckBox3: TCheckBox;
     ColorBox1: TColorBox;
+    name1: TEdit;
     Label1: TLabel;
     PageControl1: TPageControl;
     Panel1: TPanel;
     Panel2: TPanel;
+    QFGridPanelComponent1: TQFGridPanelComponent;
     QFHorizontalScrollingText1: TQFHorizontalScrollingText;
     QFRichView1: TQFRichView;
     ScrollingText1: TQFScrollingText;
@@ -34,17 +47,22 @@ type
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
     TabSheet3: TTabSheet;
+    TabSheet4: TTabSheet;
+    name2: TEdit;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
     procedure Button5Click(Sender: TObject);
     procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
     procedure CheckBox1Click(Sender: TObject);
     procedure CheckBox2Click(Sender: TObject);
     procedure CheckBox3Change(Sender: TObject);
     procedure ColorBox1Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure LabeledEdit1KeyPress(Sender: TObject; var Key: char);
+    procedure LabeledEdit2KeyPress(Sender: TObject; var Key: char);
     procedure TabControl1Change(Sender: TObject);
     //procedure UniQuery1nameGetText(Sender: TField; var aText: string;
     //  DisplayText: Boolean);
@@ -65,7 +83,8 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  PageControl1.TabIndex:=0;
+  memo2.Lines.Assign(QFGridPanelComponent1.Lines);
+  PageControl1.TabIndex:=3;
   //QFRichView1.BackImagefile:='bg.jpg';
   ScrollingText1.BackImagefile:='bg.jpg';
   //QFHorizontalScrollingText1.BackImageFile:='bg.jpg';
@@ -73,6 +92,16 @@ begin
   //UniConnection1.SpecificOptions.Values['SQL Server.Provider']:='prDirect';// .Add('SQL Server.Provider=prDirect');
   //UniConnection1.Connected:=true;
   //UniQuery1.Active:=true;
+end;
+
+procedure TForm1.LabeledEdit1KeyPress(Sender: TObject; var Key: char);
+begin
+  if key=#13 then Button7Click(self);
+end;
+
+procedure TForm1.LabeledEdit2KeyPress(Sender: TObject; var Key: char);
+begin
+  if key=#13 then Button7Click(self);
 end;
 
 procedure TForm1.TabControl1Change(Sender: TObject);
@@ -88,6 +117,10 @@ begin
   else if TabControl1.TabIndex=2 then
   begin
     PageControl1.TabIndex:=2;
+  end
+  else if TabControl1.TabIndex=3 then
+  begin
+    PageControl1.TabIndex:=3;
   end;
 end;
 
@@ -119,6 +152,19 @@ end;
 procedure TForm1.Button6Click(Sender: TObject);
 begin
   QFRichView1.PageFooter;
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+var e,h:integer;
+  gh:integer;
+begin
+  val(LabeledEdit1.text,h,e);
+  val(LabeledEdit2.text,gh,e);
+  QFGridPanelComponent1.Lines.Clear;
+  QFGridPanelComponent1.RowHeight:=h;
+  QFGridPanelComponent1.Height:=gh;
+  QFGridPanelComponent1.Lines.Assign(memo2.Lines);
+  QFGridPanelComponent1.Refresh;
 end;
 
 procedure TForm1.CheckBox1Click(Sender: TObject);
